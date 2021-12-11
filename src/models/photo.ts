95c9924c276fc7de83c6999796b5b7f12e2e4a52
fawzi bazari <fawzi.bazari2@gmail.com>
2021-12-11 15:07:32 +0100

    entity with relations finish

---
+++ src/models/photo.ts
@@ -0,0 +1,19 @@
+import {Entity, PrimaryGeneratedColumn, Column, ManyToOne} from "typeorm";
+import {User} from "./user";
+
+@Entity()
+export class Photo {
+
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    url!: string;
+    
+    @Column()
+    description!: string;
+
+    @ManyToOne(() => User, user => user.photos)
+    user!: User;
+
+}
---
+++ src/models/user.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, OneToMany, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { Photo } from "./photo";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id!: number;
+  @Column()
+  firstname!: string;
+  @Column()
+  lastname!: string;
+  @Column()
+  password!: string;
+  @Column()
+  isValid?: boolean;
+  @OneToMany(() => Photo, photo => photo.user)
+  photos!: Photo[];
+}
